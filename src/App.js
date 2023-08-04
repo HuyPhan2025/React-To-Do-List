1f6356ebc9e7de2cc457864209a350f5d68a10c2
Huy Phan <huyphan520@gmail.com>
2023-08-04 12:56:03 -0500

    Initial commit and set up

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="header"> To Do List </h1>
+      <div className="list">  </div>
+      <div className="input"> 
+        <form>
+          <input type="text" className="" />
+          <button>Submit</button>
+        </form>
+      </div>
     </div>
   );
 }
